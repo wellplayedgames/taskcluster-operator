c45571ed161638c9a77aa178e7d251e1f1c0bb95
Ricky Taylor <ricky@wellplayed.games>
2021-01-12 09:27:43 +0000

    Upgrade default image to v40.0.3

--- controllers/taskcluster_operations.go
+++ controllers/taskcluster_operations.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	defaultDockerRepo = "taskcluster/taskcluster"
-	defaultVersion    = "40.0.0"
+	defaultVersion    = "40.0.3"
 	stateKey          = "state"
 	fieldOwner        = "taskcluster.wellplayed.games"
 )
